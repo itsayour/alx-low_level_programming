355f9cec97ffb7198c0c0d3451804df014a2f803
BoulahyaMahmoud <mahmoudboulahya123@gmail.com>
2023-06-16 00:51:44 +0100

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - the entry point
+ *
+ * Return: always success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
