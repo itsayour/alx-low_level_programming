c822aa0b685d3d5878a876cf9c47c169b90244f6
BoulahyaMahmoud <mahmoudboulahya123@gmail.com>
2023-06-17 00:23:16 +0100

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * Main - return alphabetics
+ * in lower case
+ *
+ * Return: Success
+ */
+int main(void)
+{
+	for(char i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
