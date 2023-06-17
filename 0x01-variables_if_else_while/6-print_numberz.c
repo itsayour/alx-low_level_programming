fbed9324f0d57bf6fdfd7d72164cbded5b51dc86
BoulahyaMahmoud <mahmoudboulahya123@gmail.com>
2023-06-17 01:10:13 +0100

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - return alphabetics
+ * in lower case then in
+ * upercase
+ *
+ * Return: Success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
