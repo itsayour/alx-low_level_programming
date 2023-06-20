e330b2603268bc578a5feca49f78bcc10ac2d2f9
BoulahyaMahmoud <mahmoudboulahya123@gmail.com>
2023-06-21 00:43:21 +0100

    print 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,13 @@
  *
  * Return: Always 0.
  */
-void print_alphabet_x10()
+void print_alphabet_x10(void)
 {
 	int i, j;
 
-	for (i=0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j=97; j < 123; j++)
+		for (j = 97; j < 123; j++)
 		{
 			_putchar(j);
 		}
