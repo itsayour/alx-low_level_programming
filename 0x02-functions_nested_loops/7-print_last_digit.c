acb24d8854200b164cffb74b31d308c02ba12b3b
BoulahyaMahmoud <mahmoudboulahya123@gmail.com>
2023-06-21 02:55:22 +0100

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_last_digit -  function that prints the last digit of a number..
- *  @n: int type number
+ *  @c: int type number
  * Return: the last digit
  */
 
@@ -14,5 +14,5 @@ int print_last_digit(int c)
 		last_digit *= -1;
 	}
 	_putchar(last_digit + '0');
-	return last_digit;
+	return (last_digit);
 }
