2acde254b19849693954189ab5f0d3cff9564ddb
BoulahyaMahmoud <mahmoudboulahya123@gmail.com>
2023-06-22 15:25:37 +0100

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,8 @@
+/**
+ * _isupper - return 1 if the character is a uppercase, and 0 otherwise.
+ * @c: the number to be checked
+ * Return: Always 0.
+ */
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
