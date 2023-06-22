c77094cc9f8f032453190c82aa99db016f257061
BoulahyaMahmoud <mahmoudboulahya123@gmail.com>
2023-06-22 15:32:06 +0100

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - return result of mul of two numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: Always Result.
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+	return result;
+}
