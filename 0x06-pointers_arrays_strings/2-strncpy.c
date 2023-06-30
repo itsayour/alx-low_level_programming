cc679ea040b06887da9f224d9ff57f2d2cbd24ff
BoulahyaMahmoud <mahmoudboulahya123@gmail.com>
2023-06-30 23:11:12 +0100

    guilty consience

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+/**
+ * _strncpy - function that copies a string
+ *
+ * @dest: destination string
+ * @src: source srting
+ * @n: integer that specify the max num of chars to be copied.
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
