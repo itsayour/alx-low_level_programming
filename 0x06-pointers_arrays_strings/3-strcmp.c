16ff23a6dc68e23587be15b0f0073916868ea4cb
BoulahyaMahmoud <mahmoudboulahya123@gmail.com>
2023-07-01 02:23:33 +0100

    string campare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,3 +1,10 @@
+/**
+ * _strcmp - function that compares two strings.
+ *
+ * @s1: pointer of the first string to be compared.
+ * @s2: pointer of the second string to be compared.
+ * Return: integer.
+ */
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
