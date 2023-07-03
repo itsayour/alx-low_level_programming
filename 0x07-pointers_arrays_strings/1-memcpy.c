c31c30a10a55977fc5d214f3960917fc1cb4f379
BoulahyaMahmoud <mahmoudboulahya123@gmail.com>
2023-07-03 22:10:51 +0100

    what wrong with this f checker

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,14 +3,14 @@
  * _memcpy - function that copies memory area.
  * @dest: destination memory area.
  * @src: source memory ares.
- * @n: number of bytes t be copied.
+ * @n: number of bytes to be copied.
  * Return: dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
