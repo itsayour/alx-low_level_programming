5e2fc1e6ebbbacdf7f20bb3a1a1c9cab7ef036f4
BoulahyaMahmoud <mahmoudboulahya123@gmail.com>
2023-07-28 01:31:11 +0100

    free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,17 +4,20 @@
  * @head: pointer to the head of the list
  * Return: void
  */
+
 void free_list(list_t *head)
 {
 	list_t *curr = head;
 	list_t *next = head;
 
-	if(!head)
+	if (!head)
 		return;
 	while (curr)
 	{
 		next = curr->next;
+		free(curr->str);
 		free(curr);
 		curr = next;
 	}
+
 }
