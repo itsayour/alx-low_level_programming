d97f77be299f88bb11dfa2225c872f6112da597c
BoulahyaMahmoud <mahmoudboulahya123@gmail.com>
2023-08-01 16:37:16 +0100

    betty fix

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * listint_len - returns the number of elements in a linked listint_t list.
+ * @h: pointer to the first node in the list.
+ * Return: numbers of nodes.
+ */
 size_t listint_len(const listint_t *h)
 {
 	unsigned int node = 0;
