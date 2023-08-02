b5a27a2984376c71097ded29d6e5b66b40debbaf
BoulahyaMahmoud <mahmoudboulahya123@gmail.com>
2023-08-02 02:49:33 +0100

    betty fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 	curr = *head;
-	for (i = 0; i <index && curr; i++)
+	for (i = 0; i < index && curr; i++)
 	{
 		curr = curr->next;
 	}
