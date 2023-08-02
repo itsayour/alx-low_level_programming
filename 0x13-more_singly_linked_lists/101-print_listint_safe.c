c504e828979705f2f0f66711c7bc3428a72d4830
BoulahyaMahmoud <mahmoudboulahya123@gmail.com>
2023-08-02 03:09:53 +0100

    safe list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,4 +1,9 @@
 #include "lists.h"
+/**
+ * print_listint_safe - print a listint_t linked list.
+ * @head: pointer to the first node.
+ * Return:  the number of nodes in the list
+*/
 size_t print_listint_safe(const listint_t *head)
 {
 	unsigned int count = 0;
